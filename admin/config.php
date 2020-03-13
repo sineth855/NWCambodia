a529f6f5c493d57c64eab8a6e754bc4b2885b3b2
eliteht9 <61855430+eliteht9@users.noreply.github.com>
2020-03-13 13:29:46 +0700

    test
    test

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://nutritionwarehouse.local:8080/admin/');
+define('HTTP_CATALOG', 'http://nutritionwarehouse.local:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://nutritionwarehouse.local:8080/admin/');
+define('HTTPS_CATALOG', 'http://nutritionwarehouse.local:8080/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/SIM SINETH/Project/Web/personal/NWCambodia/admin/');
+define('DIR_SYSTEM', 'D:/SIM SINETH/Project/Web/personal/NWCambodia/system/');
+define('DIR_IMAGE', 'D:/SIM SINETH/Project/Web/personal/NWCambodia/image/');
+define('DIR_STORAGE', 'D:/SIM SINETH/Project/Web/personal/NWCambodia/storage/');
+define('DIR_CATALOG', 'D:/SIM SINETH/Project/Web/personal/NWCambodia/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'nutrition_warehouse');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'nw_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
