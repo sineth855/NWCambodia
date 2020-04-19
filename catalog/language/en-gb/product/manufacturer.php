7e2cfa9677a9ea22b6ebd69695570608270de742
Longdy Sim <simlongdycambodia855@gmail.com>
2020-04-19 14:48:37 +0700

    update style

--- catalog/language/en-gb/product/manufacturer.php
+++ catalog/language/en-gb/product/manufacturer.php
@@ -25,3 +25,4 @@ $_['text_rating_desc']  = 'Rating (Highest)';
 $_['text_model_asc']    = 'Model (A - Z)';
 $_['text_model_desc']   = 'Model (Z - A)';
 $_['text_limit']        = 'Show:';
+$_['button_buy_now']        = 'Buy Now';
