cef35ebd2ef192b7d621c1a3d52440106e62e01f
Longdy Sim <simlongdycambodia855@gmail.com>
2020-06-28 21:29:10 +0700

    DY

--- catalog/language/kh-gb/checkout/success.php
+++ catalog/language/kh-gb/checkout/success.php
@@ -1,9 +1,9 @@
 <?php
 // Heading
-$_['heading_title']        = 'Your order has been placed!';
+$_['heading_title']        = 'ការបញ្ជាទិញរបស់អ្នកត្រូវបានកម៉ង់ហើយ!';
 
 // Text
-$_['text_basket']          = 'Shopping Cart';
+$_['text_basket']          = 'ទិញ​ឥវ៉ាន់';
 $_['text_checkout']        = 'Checkout';
 $_['text_success']         = 'Success';
 $_['text_customer']        = '<p>Your order has been successfully processed!</p><p>You can view your order history by going to the <a href="%s">my account</a> page and by clicking on <a href="%s">history</a>.</p><p>If your purchase has an associated download, you can go to the account <a href="%s">downloads</a> page to view them.</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
